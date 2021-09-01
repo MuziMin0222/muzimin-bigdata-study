e91a5ee294aa4b67106642621a076a04aca8dcba
MuziMin0222 <1367930183@qq.com>
2021-09-01 19:42:58 +0800

    add: some exercise

---
+++ Flink1.12/src/main/scala/com/MuziMin/cdc_16/DataStreamCDCDemo.scala
@@ -0,0 +1,10 @@
+package com.MuziMin.cdc_16
+
+/**
+ * @author : 李煌民
+ * @date : 2021-08-17 22:53
+ *       ${description}
+ **/
+object DataStreamCDCDemo {
+
+}
---
+++ spark/src/main/scala/com/muzimin/exercise/CleanCampaignExcel.scala
@@ -0,0 +1,10 @@
+package com.muzimin.exercise
+
+/**
+ * @author : 李煌民
+ * @date : 2021-08-17 14:51
+ *       ${description}
+ **/
+object CleanCampaignExcel {
+
+}
