d02687d4fa2b9e82beae626ea160aaa14962bc51
muzimin <huangmin.li@mail.hypers.com>
2023-07-20 14:11:16 +0800

    feat: SpringMVC 注解请求参数demo

--- SpringMVCStudy/mvc-demo01/src/main/java/com/muzimin/controller/TestParamsController.java
+++ SpringMVCStudy/mvc-demo01/src/main/java/com/muzimin/controller/TestParamsController.java
@@ -1,9 +1,14 @@
 package com.muzimin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Arrays;
 
 /**
  * @author: 李煌民
@@ -14,6 +19,8 @@ public class TestParamsController {
 
     @RequestMapping(value = "/testServletApi")
     public String testServletApi(HttpServletRequest httpServletRequest) {
+        HttpSession session = httpServletRequest.getSession();
+
         String userName = httpServletRequest.getParameter("username");
         String password = httpServletRequest.getParameter("password");
 
@@ -26,5 +33,27 @@ public class TestParamsController {
     public String indexName() {
         //返回视图名称
         return "test_params";
+    }
+
+    @RequestMapping(value = "/testParams")
+    public String testControllerParams(String username, String password) {
+        System.out.println(username + "--->" + password);
+
+        return "success";
+    }
+
+    @RequestMapping(value = "/testParams2")
+    public String testControllerParams2(
+            @RequestParam(value = "username", required = false, defaultValue = "muzimin") String username,
+            String password,
+            String[] hobby,
+            @RequestHeader(value = "Host", required = false, defaultValue = "muzimin.com") String host,
+            @CookieValue(value = "JSESSIONID") String jsessionId
+    ) {
+        System.out.println(username + "--->" + password + "--->" + Arrays.toString(hobby));
+        System.out.println("Host:" + host);
+        System.out.println("JSESSIONID:" + jsessionId);
+
+        return "success";
     }
 }
