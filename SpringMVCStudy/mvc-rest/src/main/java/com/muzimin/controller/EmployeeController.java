d97634ade6f99f40bbd0b77d3ad7ecc32961f4d8
muzimin <huangmin.li@mail.hypers.com>
2023-08-03 00:12:36 +0800

    feat: springMVC restful crud

--- SpringMVCStudy/mvc-rest/src/main/java/com/muzimin/controller/EmployeeController.java
+++ SpringMVCStudy/mvc-rest/src/main/java/com/muzimin/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import com.muzimin.dao.EmployeeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -20,12 +21,38 @@ public class EmployeeController {
     @Autowired
     private EmployeeDao employeeDao;
 
-    @RequestMapping(value = "/getAll", method = RequestMethod.GET)
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
     public String getAll(Model model) {
         Collection<Employee> employeeDaoAll = employeeDao.getAll();
 
         model.addAttribute("employeeList", employeeDaoAll);
 
         return "employee";
+    }
+
+    @RequestMapping(value = "/employee/{id}", method = RequestMethod.DELETE)
+    public String deleteEmployee(@PathVariable("id") Integer id) {
+        employeeDao.delete(id);
+        return "redirect:/employee";
+    }
+
+    @RequestMapping(value = "/employee", method = RequestMethod.POST)
+    public String addEmployee(Employee employee) {
+        employeeDao.save(employee);
+        return "redirect:/employee";
+    }
+
+    @RequestMapping(value = "/employee/{id}", method = RequestMethod.GET)
+    public String getEmployeeById(@PathVariable("id") Integer id, Model model) {
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("employee", employee);
+
+        return "employee_update";
+    }
+
+    @RequestMapping(value = "/employee", method = RequestMethod.PUT)
+    public String updateEmployee(Employee employee) {
+        employeeDao.save(employee);
+        return "redirect:/employee";
     }
 }
